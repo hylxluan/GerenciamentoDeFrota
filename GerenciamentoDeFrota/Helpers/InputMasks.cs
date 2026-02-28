c42c9714846aec0fbf4bc5b88f995b6e7cf21b28
hylxluan <luanplays212004@gmail.com>
2026-02-28 04:26:39 -0300

    feat: finishing vehicles masks + refactor to MVVM arch

--- GerenciamentoDeFrota/Helpers/InputMasks.cs
+++ GerenciamentoDeFrota/Helpers/InputMasks.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Windows.Input;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace GerenciamentoDeFrota.Helpers
 {
@@ -23,6 +24,88 @@ namespace GerenciamentoDeFrota.Helpers
                 else
                 {
                     e.Handled = true;
+                }
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        public static void LimitarCaracteresNumericos_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (sender is not TextBox renavam) return;
+
+            e.Handled = renavam.Text.Length >= 11 || !int.TryParse(e.Text, out _);
+        }
+
+        public static void PlacaMascara_TextChanged(object sender, TextChangedEventArgs e) 
+        { 
+            if (sender is not TextBox placa) return;
+
+            int posicaoInicial = placa.SelectionStart;
+
+            string placaFormatada = new string(placa.Text.ToUpper().Where(character => char.IsLetterOrDigit(character)).ToArray());
+
+            if (placaFormatada.Length > 7) placaFormatada = placaFormatada.Substring(0, 7);
+
+
+            var result = "";
+
+
+            for (int i = 0; i < placaFormatada.Length; i++)
+            {
+                char c = placaFormatada[i];
+
+                switch (i) 
+                {
+                    case 0:
+                    case 1:
+                    case 2:
+                        if (char.IsLetter(c)) result += c;
+                        break;
+                    case 3:
+                            if (char.IsDigit(c)) result += c;
+                            break;
+                    case 4:
+                        if (char.IsLetter(c)) result += c;
+                        break;
+                    case 5:
+                    case 6:
+                        if (char.IsDigit(c)) result += c;
+                        break;
+                }
+
+            }
+            if (placa.Text != result)
+            {
+                placa.Text = result;
+                placa.SelectionStart = posicaoInicial > placa.Text.Length ? placa.Text.Length : posicaoInicial;
+            }
+        }
+
+        public static void LimitarAno_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (sender is TextBox txtAno)
+            {
+                if (!int.TryParse(e.Text, out _))
+                {
+                    e.Handled = true;
+                    return;
+                }
+
+                if (txtAno.Text.Length >= 4)
+                {
+                    e.Handled = true;
+                    return;
+                }
+
+                string newText = txtAno.Text + e.Text;
+                if (newText.Length == 4 && int.TryParse(newText, out int ano))
+                {
+                    int anoAtual = DateTime.Now.Year;
+                    int anoMinimo = anoAtual - 60;
+                    e.Handled = ano > anoAtual || ano < anoMinimo;
                 }
             }
             else
---
+++ GerenciamentoDeFrota/Views/VeiculosView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GerenciamentoDeFrota.Helpers;
+
+namespace GerenciamentoDeFrota.Views
+{
+    /// <summary>
+    /// Interaction logic for VeiculosView.xaml
+    /// </summary>
+    public partial class VeiculosView : UserControl
+    {
+        public VeiculosView()
+        {
+            InitializeComponent();
+            AplicarMasacaras();
+        }
+
+        private void AplicarMasacaras() 
+        {
+            TxtPlaca.TextChanged += InputMasks.PlacaMascara_TextChanged;
+            TxtAnoModelo.PreviewTextInput += InputMasks.LimitarAno_PreviewTextInput;
+            TxtAnoFabricacao.PreviewTextInput += InputMasks.LimitarAno_PreviewTextInput;
+            TxtMesEmplacamento.PreviewTextInput += InputMasks.LimitarMesEmplacamento_PreviewTextInput;
+            TxtRenavam.PreviewTextInput += InputMasks.LimitarCaracteresNumericos_PreviewTextInput;
+        }
+    }
+}

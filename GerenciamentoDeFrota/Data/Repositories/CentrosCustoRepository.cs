15e9fdd18ed9fa77233a234b61150cd87e08c883
hylxluan <luanplays212004@gmail.com>
2026-03-05 15:57:55 -0300

    feat: CentrosCusto repository finished

--- GerenciamentoDeFrota/Data/Repositories/CentrosCustoRepository.cs
+++ GerenciamentoDeFrota/Data/Repositories/CentrosCustoRepository.cs
@@ -4,36 +4,53 @@ using System.Text;
 using GerenciamentoDeFrota.Interfaces.Repositories;
 using GerenciamentoDeFrota.Data.Models;
 using GerenciamentoDeFrota.Configs;
+using GerenciamentoDeFrota.Exceptions.CustomExceptions;
 
 namespace GerenciamentoDeFrota.Data.Repositories
 {
     public class CentrosCustoRepository : ICentrosCustoRepository
     {
 
-        private readonly AppDbContext? _context;
+        private readonly AppDbContext _context;
 
+        public CentrosCustoRepository(AppDbContext dbContext)
+        {
+            this._context = dbContext;
+        }
 
         public void AddCentroCusto(CentrosCusto centroCusto)
         {
-            throw new NotImplementedException();
+            _context.CentrosCusto.Add(centroCusto);
+            _context.SaveChanges();
         }
 
         public void DeleteCentroCusto(long id)
         {
-            throw new NotImplementedException();
+            var entity = GetCentroCustoById(id);
+            if (entity != null) 
+            {
+                _context.CentrosCusto.Remove(entity);
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new RegisterNotFoundException("Centro de custo não encontrado para exclusão!");
+            }
+
         }
 
-        public CentrosCusto? GetCentroCustoById(long id)
-        {
-            throw new NotImplementedException();
-        }
+        public CentrosCusto? GetCentroCustoById(long id) => 
+            _context.CentrosCusto.FirstOrDefault(c => c.Id == id);
 
         public List<CentrosCusto> GetCentrosCustos() =>
             _context.CentrosCusto.OrderBy(c => c.Nome).ThenByDescending(c => c.DataCriacao).ToList();
 
         public void UpdateCentroCusto(CentrosCusto centroCusto)
         {
-            throw new NotImplementedException();
+            _context.CentrosCusto.Update(centroCusto);
+            _context.SaveChanges();
         }
+
+        
     }
 }

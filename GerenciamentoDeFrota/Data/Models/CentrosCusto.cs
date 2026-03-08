e50e6dbfdb1a39933ddec258265cf90cfc43121b
hylxluan <luanplays212004@gmail.com>
2026-03-08 18:25:49 -0300

    refactor: centros custo model with nullable types feat: Veiculos model

--- GerenciamentoDeFrota/Data/Models/CentrosCusto.cs
+++ GerenciamentoDeFrota/Data/Models/CentrosCusto.cs
@@ -8,9 +8,9 @@ namespace GerenciamentoDeFrota.Data.Models
     public class CentrosCusto
     {
         public long Id { get; set; }
-        public string Nome { get; set; } = string.Empty;
-        public string Observacoes { get; set; } = string.Empty;
-        public bool Ativo { get; set; } = true;
-        public DateTime DataCriacao { get; set; } = DateTime.UtcNow.Date;
+        public string? Nome { get; set; } = string.Empty;
+        public string? Observacoes { get; set; } = string.Empty;
+        public bool? Ativo { get; set; } = true;
+        public DateTime? DataCriacao { get; set; } = DateTime.UtcNow.Date;
     }
 }
--- GerenciamentoDeFrota/Data/Models/Veiculos.cs
+++ GerenciamentoDeFrota/Data/Models/Veiculos.cs
@@ -7,7 +7,20 @@ namespace GerenciamentoDeFrota.Data.Models
     [Serializable]
     public class Veiculos
     {
-
+        public long Id { get; set; }
+        public string? Fabricante { get; set; } = string.Empty;
+        public string? Modelo { get; set; } = string.Empty;
+        public int? AnoModelo { get; set; }
+        public int? AnoFabricacao { get; set; }
+        public bool? Ativo { get; set; } = true;
+        public string? Renavam { get; set; } = string.Empty;
+        public string? Placa { get; set; } = string.Empty;
+        public int? MesEmplacamento { get; set; }
+        public int? AnoEmplacamento { get; set; }
+        public DateTime? DataTacografo { get; set; } = DateTime.UtcNow.Date;
+        public string? Cor { get; set; } = string.Empty;
+        public string? Observacoes { get; set; } = string.Empty;
+        public DateTime? DataCriacao { get; set; } = DateTime.UtcNow.Date;
 
 
     }

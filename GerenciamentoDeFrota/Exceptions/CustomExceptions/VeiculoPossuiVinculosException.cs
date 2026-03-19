d548fb5c09f63cee4f4bbf4f32c734c2898ac317
hylxluan <luanplays212004@gmail.com>
2026-03-19 02:28:49 -0300

    feat: Exceptions for block delete vehicles if it is related with some schedule

---
+++ GerenciamentoDeFrota/Exceptions/CustomExceptions/VeiculoPossuiVinculosException.cs
@@ -0,0 +1,15 @@
+﻿using GerenciamentoDeFrota.Exceptions.ExceptionBase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciamentoDeFrota.Exceptions.CustomExceptions
+{
+    public class VeiculoPossuiVinculosException : GerenciamentoDeFrotaExceptions
+    {
+        public int? TotalAgendamentos { get; }
+        public VeiculoPossuiVinculosException(int? totalAgendamentos) : 
+            base($"Este veículo tem um total de {totalAgendamentos} agendamentos relacionados!")
+        => TotalAgendamentos = totalAgendamentos;
+    }
+}

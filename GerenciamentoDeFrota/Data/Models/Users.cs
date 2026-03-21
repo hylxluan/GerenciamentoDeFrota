59bfcecbd864fc9a4a21ef9b3dfc3fff994cae93
hylxluan <luanplays212004@gmail.com>
2026-03-21 02:02:35 -0300

    feat: validator + new sql server on azure

---
+++ GerenciamentoDeFrota/Data/Models/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciamentoDeFrota.Data.Models
+{
+    [Serializable]
+    public class Users
+    {
+        private int Id { get; set; }
+        private string? Name { get; set; }
+        private string? Email { get; set; }
+        private string? login { get; set; }
+        private string? password { get; set; }
+        private bool? Ativo { get; set; }
+        private int? IDOficina { get; set; }
+        private int? IDGerenciadora { get; set; }
+        private int? IDCondutor { get; set; }
+        private DateTime? CreatedAt { get; set; }
+        private DateTime? UpdatedAt { get; set; }
+        
+
+    }
+}
---
+++ GerenciamentoDeFrota/Data/Services/Validators/VeiculosValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using GerenciamentoDeFrota.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciamentoDeFrota.Data.Services.Validators
+{
+    public class VeiculosValidator : AbstractValidator<Veiculos>
+    {
+        public VeiculosValidator()
+        {
+            
+        }
+
+    }
+}

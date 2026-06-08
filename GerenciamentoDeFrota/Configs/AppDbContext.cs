3ff0b3b1da67f9addd90b6c822a27be8777d64ac
hylxluan <luanplays212004@gmail.com>
2026-06-08 15:54:33 -0300

    feat: Trava de segurança de deleções em cascata

--- GerenciamentoDeFrota/Configs/AppDbContext.cs
+++ GerenciamentoDeFrota/Configs/AppDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using GerenciamentoDeFrota.Data.Models;
 using Microsoft.Extensions.Configuration;
-using System;
 
 namespace GerenciamentoDeFrota.Configs
 {
@@ -9,6 +8,8 @@ namespace GerenciamentoDeFrota.Configs
     {
         public DbSet<CentrosCusto> CentrosCusto { get; set; }
         public DbSet<Veiculos> Veiculos { get; set; }
+        public DbSet<VeiculoDocumento> VeiculosDocumentos { get; set; }
+        public DbSet<VeiculoAnexo> VeiculosAnexos { get; set; }
         public DbSet<AgendamentoManutencao> AgendamentosManutencao { get; set; }
 
         public AppDbContext() { }
@@ -24,11 +25,47 @@ namespace GerenciamentoDeFrota.Configs
                     .AddJsonFile("appsettings.json", optional: false)
                     .Build();
 
-                optionsBuilder.UseMySQL(config.GetConnectionString("Default"));
+                optionsBuilder.UseMySQL(config.GetConnectionString("Default")!);
             }
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder) => 
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // não apaga o centro se tiver veículo vinculado
+            modelBuilder.Entity<Veiculos>()
+                .HasOne(v => v.CentrosCusto)
+                .WithMany(c => c.Veiculos)
+                .HasForeignKey(v => v.CentrosCustoId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // apaga os documentos junto com o veículo
+            modelBuilder.Entity<VeiculoDocumento>()
+                .HasOne(d => d.Veiculo)
+                .WithMany(v => v.Documentos)
+                .HasForeignKey(d => d.VeiculoId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // apaga os anexos junto com o veículo
+            modelBuilder.Entity<VeiculoAnexo>()
+                .HasOne(a => a.Veiculo)
+                .WithMany(v => v.Anexos)
+                .HasForeignKey(a => a.VeiculoId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // não apaga o veículo se tiver agendamento
+            modelBuilder.Entity<AgendamentoManutencao>()
+                .HasOne(a => a.Veiculo)
+                .WithMany()
+                .HasForeignKey(a => a.VeiculoId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // não persiste
+            modelBuilder.Entity<Veiculos>().Ignore(v => v.VeiculoDescricao);
+            modelBuilder.Entity<AgendamentoManutencao>().Ignore(a => a.VeiculoDescricao);
+            modelBuilder.Entity<AgendamentoManutencao>().Ignore(a => a.HoraFormatada);
+            modelBuilder.Entity<AgendamentoManutencao>().Ignore(a => a.KmFormatado);
+        }
     }
 }
